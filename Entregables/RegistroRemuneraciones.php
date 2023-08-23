02b28f1986d80eb911bc67933b93d2c6fcaf0b15
Superlili15 <73138582+Superlili15@users.noreply.github.com>
2023-08-23 13:26:49 -0400

    + archivos
    la carga masiva es de prueba, la carpeta del lector de XLSX es un proyecto git aparte, el link esta en la 1ra linea de RegistroRemuneraciones.php
    (aun no hay un sistema que evite la repeticion porque estorbaria en las prubeas)

---
+++ Entregables/RegistroRemuneraciones.php
@@ -0,0 +1,85 @@
+<?php //se necesita https://github.com/PHPOffice/PHPExcel
+require_once 'PHPExcel-1.8/Classes/PHPExcel.php';
+$archivo = "Carga Masiva REM y ejemplo.xlsx";
+$inputFileType = PHPExcel_IOFactory::identify($archivo);
+$objReader = PHPExcel_IOFactory::createReader($inputFileType);
+$objPHPExcel = $objReader->load($archivo);
+$sheet = $objPHPExcel->getSheet(1); 
+$highestRow = $sheet->getHighestRow(); 
+$highestColumn = $sheet->getHighestColumn();
+
+$data = file_get_contents("key.json");
+$key = json_decode($data, true);
+$usuario = $key[ 'usuario' ];
+$password = $key[ 'password' ];
+$servidor = $key[ 'servidor' ];
+$basededatos = $key[ 'basededatos' ];
+$PKempresa = "867";
+$a =0;
+$conexion = mysqli_connect($servidor, $usuario, $password, $basededatos) or die ("No se ha podido conectar al servidor de Base de datos");
+$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+
+for ($row = 2; $row <= $highestRow; $row++){ 
+$boolean= TRUE; //no existe
+$pk_depto = -1;
+while($boolean){
+$consulta = "SELECT * FROM `departamento`";
+$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos 1er aviso");
+// $columna = mysqli_fetch_array( $resultado );
+
+while ($columna = mysqli_fetch_array( $resultado))
+{
+    $IDC = $columna['Nombre'] ;
+    if($IDC == $sheet->getCell("B".$row)->getValue()){
+    $boolean=FALSE;
+    $pk_depto = $columna['PK'] ;
+    }
+}
+if($boolean){
+  $consulta2 = "INSERT INTO departamento(PK,Nombre) VALUES ('0','".$sheet->getCell("B".$row)->getValue() ."')";
+  $resultado = mysqli_query( $conexion, $consulta2 ) or die ( "Algo ha ido mal en la consulta a la base de datos 2do aviso");
+}}
+$boolean= TRUE; //no existe
+$pk_trab = -1;
+while($boolean){
+$consulta = "SELECT * FROM `trabajadores`";
+$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos 1er aviso");
+// $columna = mysqli_fetch_array( $resultado );
+
+while ($columna = mysqli_fetch_array( $resultado))
+{
+    $IDC = $columna['RUT'] ;
+    if($IDC == $sheet->getCell("D".$row)->getValue()){
+    $boolean=FALSE;
+    $pk_trab = $columna['PK'] ;
+    }
+}
+if($boolean){
+    echo "no lo encontro";
+    $pk_trab = '1' ;
+}}
+$consulta3 =  "INSERT INTO `a_trabajo`.`remuneraciones` (`PK`, `Depto`, `Cod`, `Trabajador`, 
+`DT`, `S.BASE`, `GRAT.LEGAL`, `VALOR IMP.`, `TOTAL IMP.`, `ASIG. FAM.`, `CONECT.`, `MOVI,`, 
+`COLACION`, `TOTAL NO IMP`, `TOT. HABERES`, `PREVISION`, `SALUD`, `IMP.UNICO`, `SEG.CES.`, 
+`ADIC. ISAPRE`, `TOT. DESC.LEG.`, `RET SII`, `TOT.DESC`, `LIQUIDO`, `SIS`, `AFC`, `MUTUAL`, 
+`COSTO EMPRESA`) VALUES ('0', '".$pk_depto."', '".$sheet->getCell("C".$row)->getValue()."', 
+'".$pk_trab."', '".$sheet->getCell("F".$row)->getValue()."', '".$sheet->getCell("G".$row)->getValue()."',
+ '".$sheet->getCell("H".$row)->getValue()."', '".$sheet->getCell("I".$row)->getValue()."', 
+ '".$sheet->getCell("J".$row)->getValue()."', '".$sheet->getCell("K".$row)->getValue()."',
+  '".$sheet->getCell("L".$row)->getValue()."', '".$sheet->getCell("M".$row)->getValue()."', 
+  '".$sheet->getCell("N".$row)->getValue()."', '".$sheet->getCell("O".$row)->getValue()."', 
+  '".$sheet->getCell("P".$row)->getValue()."', '".$sheet->getCell("Q".$row)->getValue()."',
+   '".$sheet->getCell("R".$row)->getValue()."', '".$sheet->getCell("S".$row)->getValue()."', 
+   '".$sheet->getCell("T".$row)->getValue()."', '".$sheet->getCell("U".$row)->getValue()."', 
+   '".$sheet->getCell("V".$row)->getValue()."', '".$sheet->getCell("W".$row)->getValue()."',
+    '".$sheet->getCell("X".$row)->getValue()."', '".$sheet->getCell("Y".$row)->getValue()."', 
+    '".$sheet->getCell("Y".$row)->getValue()."', '".$sheet->getCell("Z".$row)->getValue()."',
+     '".$sheet->getCell("AA".$row)->getValue()."', '".$sheet->getCell("AB".$row)->getValue()."');";
+$resultado3 = mysqli_query( $conexion, $consulta3 ) or die ( "Algo ha ido mal en la consulta a la base de datos 3er aviso");
+
+echo "<a>registrado</a>";
+    }
+    
+mysqli_close( $conexion );
+  
+?>
---
+++ Entregables/RegistroVentaLibro.php
@@ -0,0 +1,39 @@
+<?php
+
+$PKempresa = "867";
+
+$data = file_get_contents("key.json");
+$key = json_decode($data, true);
+$usuario = $key[ 'usuario' ];
+$password = $key[ 'password' ];
+$servidor = $key[ 'servidor' ];
+$basededatos = $key[ 'basededatos' ];
+$conexion = mysqli_connect($servidor, $usuario, $password, $basededatos) or die ("No se ha podido conectar al servidor de Base de datos");
+$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+
+$boolean= TRUE; //no existe
+$pk_tipoempresa = -1;
+$consulta = "SELECT `compraventa`.*, `codigosdocumentos`.`Archivo` FROM `compraventa`
+ LEFT JOIN `a_trabajo`.`codigosdocumentos` ON `compraventa`.`Documento` = `codigosdocumentos`.`PK` 
+ WHERE `codigosdocumentos`.`Archivo` = 1"; //1 es Venta 2 es Compra
+$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos 1er aviso");
+// $columna = mysqli_fetch_array( $resultado );
+
+  while ($columna = mysqli_fetch_array( $resultado))
+  {
+$consulta3 = "INSERT INTO `a_trabajo`.`libro` (`PK`, `FK_empresacliente`, `FECHA`, `TIPO`, 
+`NUM`, `CUENTA`, `Sucursal`, `CentroCostos`, `GLOSA`, `DEBE`, `HABER`, `PK_Origen`, `Tabla_Origen`) 
+VALUES ('0', '".$PKempresa."', '".$columna['detFchDoc']."', '2', '".$columna['detNroDoc']."', '5',
+ NULL, NULL, 'Boleta ".$columna['PK']." Venta', NULL, '".$columna['detMntTotal']."','".$columna['PK']."', '1');";
+$resultado3 = mysqli_query( $conexion, $consulta3 ) or die ( "Algo ha ido mal en la consulta a la base de datos 3er aviso");
+
+$consulta4 = "INSERT INTO `a_trabajo`.`libro` (`PK`, `FK_empresacliente`, `FECHA`, `TIPO`, 
+`NUM`, `CUENTA`, `Sucursal`, `CentroCostos`, `GLOSA`, `DEBE`, `HABER`, `PK_Origen`, `Tabla_Origen`) 
+VALUES ('0', '".$PKempresa."', '".$columna['detFchDoc']."', '2', '".$columna['detNroDoc']."', '198',
+ NULL, NULL, 'Boleta ".$columna['PK']." Reflejo', '".$columna['detMntTotal']."', NULL,'".$columna['PK']."', '2');";
+$resultado4 = mysqli_query( $conexion, $consulta4 ) or die ( "Algo ha ido mal en la consulta a la base de datos 4to aviso");
+      }
+
+echo "<a>registrado</a>";
+mysqli_close( $conexion );
+?>
